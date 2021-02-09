363315bdf7c85f457697e6759bc7c6f3e1e7bc91
Artem Senin <artem.senin@nordigy.ru>
2021-02-09 20:34:42 +0300

    feat: observe method to add elements to IntersectionObserver's watch list

--- packages/ngx-detach-invisible/src/lib/detach-invisible.service.ts
+++ packages/ngx-detach-invisible/src/lib/detach-invisible.service.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Inject, Injectable, OnDestroy, Optional} from '@angular/core';
+import {ChangeDetectorRef, ElementRef, Inject, Injectable, OnDestroy, Optional} from '@angular/core';
 import {
   DEFAULT_DETACH_INVISIBLE_CONFIG,
   DETACH_INVISIBLE_CONFIG,
@@ -33,6 +33,22 @@ export class DetachInvisibleService implements OnDestroy {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore allow IntersectionObserver to be garbage collected
     this.observer = null;
+  }
+
+  /**
+   *
+   * @param target - Component to keep track of
+   * @param cdr - `ChangeDetectorRef` which controls change detection on
+   * @return unobserve function to remove target from `IntersectionObserver`'s watch list
+   */
+  public observe({ nativeElement: target }: ElementRef<Element>, cdr: ChangeDetectorRef): () => void {
+    const unobserve = () => this.observer.unobserve(target);
+    if (this.observables.has(target)) { return unobserve; }
+
+    this.observer.observe(target)
+    this.observables.set(target, cdr);
+
+    return unobserve;
   }
 
   private processEntries(entries: IntersectionObserverEntry[]): void {
