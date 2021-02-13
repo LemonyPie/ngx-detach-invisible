a6cb8428969e85bd3ebc288ce20b649e6b11dcf5
Artem Senin <artem.senin@nordigy.ru>
2021-02-13 04:21:21 +0300

    fix: change public_api to index file

--- packages/ngx-detach-invisible/src/index.ts
+++ packages/ngx-detach-invisible/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/detach-invisible.service';
 export * from './lib/detach-invisible.const';
 export * from './lib/detach-invisible.module';
+export * from './lib/detach-invisible.directive'
+export * from './lib/detach-invisible.types'
